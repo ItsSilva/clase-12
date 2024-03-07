c65d35ee1d987383f56f91220ea432a01232ea17
ItsSilva <juanse909@hotmail.com>
2024-03-07 07:37:39 -0500

    clase-12

---
+++ script.js
@@ -0,0 +1,15 @@
+const animal = prompt("Ingrese un animal");
+
+if (animal.toLowerCase() === "perro"){
+    alert("Mi animal es un perro");
+} else if (animal.toLowerCase() === "gato"){
+    alert("Mi animal es un gato");
+} else if (animal.toLowerCase() === "conejo"){
+    alert("Mi animal es un conejo");
+}else{
+    alert("No conozco ese animal");
+}
+
+const parrafo = document.querySelector("#parrafo");
+console.log (parrafo);
+parrafo.innerHTML = "Este es el nuevo contenido del parrafo"
